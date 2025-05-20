5f43991773231cf9414a151ccc6cae30ba915e75
JayG003 <warejay003@gmail.com>
2025-05-20 18:20:35 +0530

    update 8th ncode

---
+++ ASCII.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    char a; // variable for character
+
+    printf("Enter any character =");
+    scanf("%c", &a);
+
+    printf("previous character is =%d\n", a);
+}
