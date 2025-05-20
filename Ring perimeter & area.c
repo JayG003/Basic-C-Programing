fe716c70b940bf96ba83c9be513345a3cb82d7d5
JayG003 <warejay003@gmail.com>
2025-05-20 17:42:52 +0530

    update 4th code

---
+++ Ring perimeter & area.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#define pi 3.14 // used define for value of pi
+int main()
+{
+    int ir, or; // ir = inner radius,or = outer radius
+    printf("Enter inner radius of Ring =");
+    scanf("%d", &ir); // accepots the inner radius
+    printf("Enter outer radius of Ring =");
+    scanf("%d", &or);                          // accepots the outer radius
+    float peri = 2 * pi * (ir + or);           // formula for perimeter of ring
+    float area = pi * ((or * or) - (ir * ir)); // formula for area of ring
+    printf("Area of ring is = %.2f\n", area);
+    printf("Perimeter of Ring is = %.2f", peri);
+}
